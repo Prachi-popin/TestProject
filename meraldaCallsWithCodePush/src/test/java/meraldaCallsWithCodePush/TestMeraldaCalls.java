1b1e0a940359643b4ff0a3c2f365e10e27f7bf8f
Prachi-popin <prachi.verma@popin.to>
2025-05-29 12:21:56 +0530

    My First Commit

---
+++ meraldaCallsWithCodePush/src/test/java/meraldaCallsWithCodePush/TestMeraldaCalls.java
@@ -0,0 +1,14 @@
+package meraldaCallsWithCodePush;
+
+import org.testng.annotations.Test;
+
+public class TestMeraldaCalls {
+	
+	
+	@Test
+	public void testMeraldaCalls() {
+		
+		System.out.println("..Meralda calls.!");
+	}
+
+}
